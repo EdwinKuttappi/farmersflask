49371b39635f98652c6a88328f7f8de25d3e2bbc
EdwinKuttappi <edwinab0007@gmail.com>
2023-02-13 13:22:11 -0800

    run .py

--- projects/templates/bp_projects/maps.py
+++ projects/templates/bp_projects/maps.py
@@ -1,4 +1,19 @@
 import requests
+import random 
+
+
+<html>
+</html>
+
+print("Please choose your first airport! (The Departure Flight)")
+location1 = input()
+
+print("Please choose your second airport! (The Arrival Flight)")
+location2 = input()
+
+locations = [location1, location2]
+
+
 
 url = "https://maptiles.p.rapidapi.com/es/map/v1/3/4/2.png"
 
@@ -10,3 +25,11 @@ headers = {
 response = requests.request("GET", url, headers=headers)
 
 print(response.text)
+
+for maps in map()
+	while 
+
+
+
+<script>
+const Maps
