ac384d0349304c50b7d29d63a761d3b0f1f29ad0
EdwinKuttappi <edwinab0007@gmail.com>
2023-02-27 12:42:29 -0800

    maps

--- api/maps.py
+++ api/maps.py
@@ -7,7 +7,7 @@ maps_api = Blueprint('maps_api', __name__,
                    url_prefix='/api/maps')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
-mapsapi = Api(maps_api)
+api = Api(maps_api)
 
 class MapsPostAPI(Resource):        
     class _Create(Resource):
@@ -15,18 +15,12 @@ class MapsPostAPI(Resource):
             ''' Read data for json body '''
             body = request.get_json()
             
-            ''' Avoid garbage in, error checking '''
-            # validate name
-            location = body.get('location')
-            if location is None or len(location) < 2 or len(location) > 30:
-                return {'message': f'Name is missing, or is less than 2 characters, or is more than 30 characters'}, 210
-            
-            ''' #1: Key code block, setup USER OBJECT '''
-            uo = mapsPost(title=title, text=text, imageURL=imageURL)
-            
-            ''' Additional garbage error checking '''
+            location1 = body.get('location1')
+            location2 = body.get('location2')
 
-            
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = MapsPost(location1=location1, location2=location2)
+
             ''' #2: Key Code block to add user to database '''
             # create user in database
             user = uo.create()
@@ -34,14 +28,14 @@ class MapsPostAPI(Resource):
             if user:
                 return jsonify(user.read())
             # failure returns error
-            return {'message': f'Processed {title}, format error'}, 210
+            return {'message': f'Processed {location1}, format error'}, 210
 
     class _Read(Resource):
         def get(self):
-            users = FdPost.query.all()    # read/extract all users from database
+            users = MapsPost.query.all()    # read/extract all users from database
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
     # comment
     # building RESTapi endpoint
-    api.add_resource(_Create, '/post')
+    api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
